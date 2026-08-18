5131c90022e504dc117896582c72ae88de85efc9
sihyun <kitty13257@naver.com>
2026-08-19 04:45:03 +0900

    Delete drizzle/0000_demonic_randall.sql

--- drizzle/0000_demonic_randall.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `recommendations` (
-	`cafe_id` text NOT NULL,
-	`voter_id` text NOT NULL,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	PRIMARY KEY(`cafe_id`, `voter_id`)
-);
---> statement-breakpoint
-CREATE INDEX `idx_recommendations_cafe_id` ON `recommendations` (`cafe_id`);
